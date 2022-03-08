3442b3f314007d51e19e77a7b3afeef89dc1adaf
Setepenre <pierre.delaunay@outlook.com>
2022-03-08 09:27:49 -0500

    Add License

--- setup.py
+++ setup.py
@@ -1,20 +1,45 @@
 #!/usr/bin/env python
-from setuptools import setup
+# -*- coding: utf-8 -*-
+"""Installation script for Sample space."""
 
+from setuptools import setup
+import os
+
+repo_root = os.path.dirname(os.path.abspath(__file__))
+
+
+args = dict(
+    name='sspace',
+    version='0.0.0',
+    description='Sample Space Builder',
+    long_description=open(
+        os.path.join(repo_root, "README.rst"), "rt", encoding="utf8"
+    ).read(),
+    author='Pierre Delaunay',
+    author_email="pierre.delaunay@mila.quebec",
+    url="https://github.com/epistimio/sspace",
+    packages=[
+        'sspace',
+        'sspace.backends'
+    ],
+    setup_requires=['setuptools'],
+    install_requires=[
+        'ConfigSpace'
+    ]
+)
+
+args["classifiers"] = [
+    "Development Status :: 1 - Planning",
+    "Intended Audience :: Developers",
+    "Intended Audience :: Education",
+    "Intended Audience :: Science/Research",
+    "License :: OSI Approved :: BSD License",
+    "Operating System :: POSIX",
+    "Operating System :: Unix",
+    "Programming Language :: Python",
+    "Topic :: Scientific/Engineering",
+    "Topic :: Scientific/Engineering :: Artificial Intelligence",
+] + [("Programming Language :: Python :: %s" % x) for x in "3 3.7 3.8 3.9 3.10".split()]
 
 if __name__ == '__main__':
-    setup(
-        name='sspace',
-        version='0.0.0',
-        description='Sample Space Builder',
-        author='Pierre Delaunay',
-        packages=[
-            'sspace',
-            'sspace.backends'
-        ],
-        setup_requires=['setuptools'],
-        install_requires=[
-            'ConfigSpace',
-            'orion',
-        ]
-    )
+    setup(**args)
